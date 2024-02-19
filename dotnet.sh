27c4fc7b1801b93c6f8851462628bf1d33890eef
Brigthain kargong <kbrigthain@gmail.com>
2024-02-18 22:47:25 -0500

    Final code

--- dotnet.sh
+++ dotnet.sh
@@ -2,13 +2,33 @@
 ########################################
 #### INSTALLING THE DOTNET WEBSITE ######
 ########################################
-sudo su - 
+Webserver_path="market"
+Url="https://www.free-css.com/assets/files/free-css-templates/download/page281/dotnet.zip"
+Home_path="/var/www/html/$Webserver_path"
+
+#Update and install apache
 yum update -y
 yum install -y httpd
+
+#Create the directory
+mkdir "$Home_path"
 mkdir temp
 cd temp
-wget https://www.free-css.com/assets/files/free-css-templates/download/page281/dotnet.zip
-unzip dotnet.zip
-mv digital-agency-website-template/* /var/www/html/
+
+# Download and unzip the url
+wget "$Url"
+
+# Extract the filename from the URL
+zipped_file=$(basename "$Url")
+
+# Unzip the zipped file
+unzip "$zipped_file"
+# unzip dotnet.zip
+
+Unzipped_directory=$(ls -d */)
+mv "$Unzipped_directory"/* "$Home_path"
+sed -i "s|DocumentRoot \"/var/www/html\"|DocumentRoot \"/var/www/html/\"|" /etc/httpd/conf/httpd.conf
+
+#Start and enable apache 
 systemctl enable httpd
-systemctl start httpd
+systemctl restart httpd
